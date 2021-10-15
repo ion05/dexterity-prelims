ec32d4acec0d9c7b1650454e4609ce5459a4a151
unknown <ynarula678@gmail.com>
2021-10-15 17:19:05 +0530

    some frontend

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+const [
+    user1,
+    user2,
+    user3,
+    user4
+] = [
+    document.querySelector('.user-1'),
+    document.querySelector('.user-2'),
+    document.querySelector('.user-3'),
+    document.querySelector('.user-4')
+]
+user1.addEventListener('click',()=>{
+    console.log('object')
+})
+user2.addEventListener('click',()=>{
+    console.log('object')
+})
+
+user3.addEventListener('click',()=>{
+    console.log('object')
+})
+user4.addEventListener('click',()=>{
+    console.log('object')
+})
