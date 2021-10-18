5d53f4ad9332a1092aaa916c7ba7c852a28cf2b5
unknown <ynarula678@gmail.com>
2021-10-18 12:47:27 +0530

    index fix

--- static/js/app.js
+++ static/js/app.js
@@ -10,17 +10,66 @@ const [
     document.querySelector('.user-4')
 ]
 user1.addEventListener('click',()=>{
-    console.log('object')
+  document.querySelector('.user-reviews').innerHTML=`
+  <h2>
+  Awesome Job
+</h2>
+<div class="line-user"></div>
+<h3>User</h3>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing <br>
+  elit. Nam quis sed recusandae, alias facere sapiente <br>
+  iusto modi eaque officia nobis obcaecati minima! <br>
+  <br><br>
+  Dolorem illum nisi hic. Amet odit facilis a.
+</p>`;
 })
 user2.addEventListener('click',()=>{
-    console.log('object')
+  document.querySelector('.user-reviews').innerHTML=`
+  <h2>
+  Great Work
+</h2>
+<div class="line-user"></div>
+<h3>User</h3>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing <br>
+  elit. Nam quis sed recusandae, alias facere sapiente <br>
+  iusto modi eaque officia nobis obcaecati minima! <br>
+  <br><br>
+  Dolorem illum nisi hic. Amet odit facilis a.
+</p>
+  `
 })
-
 user3.addEventListener('click',()=>{
-    console.log('object')
+  document.querySelector('.user-reviews').innerHTML = `
+  <h2>
+  Superb
+</h2>
+<div class="line-user"></div>
+<h3>User</h3>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing <br>
+  elit. Nam quis sed recusandae, alias facere sapiente <br>
+  iusto modi eaque officia nobis obcaecati minima! <br>
+  <br><br>
+  Dolorem illum nisi hic. Amet odit facilis a.
+</p>
+  `
 })
 user4.addEventListener('click',()=>{
-    console.log('object')
+  document.querySelector('.user-reviews').innerHTML = `
+  <h2>
+ Great work
+</h2>
+<div class="line-user"></div>
+<h3>User</h3>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing <br>
+  elit. Nam quis sed recusandae, alias facere sapiente <br>
+  iusto modi eaque officia nobis obcaecati minima! <br>
+  <br><br>
+  Dolorem illum nisi hic. Amet odit facilis a.
+</p>`
 })
 var btn = $('#button');
 
